49656a5cc081af546efa9e84d00cff638696eb55
knta <knta.dev@gmail.com>
2021-02-14 20:23:13 +0100

    Adding more logic

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
+import tkinter.font as tkFont
 from functools import partial
 
 WIDTH = 265
-HEIGHT = 427
+HEIGHT = 450
 GEOMETRY = "{}x{}".format(WIDTH, HEIGHT)
 BACKGROUND_COLOR = "#181717"
 
@@ -21,6 +22,7 @@ class Calculator(tk.Frame):
 
         self.current_number = tk.StringVar()
         self.aux_number = ""
+        self.op = ""
 
         self.create_ui()
 
@@ -28,7 +30,8 @@ class Calculator(tk.Frame):
         
         # Display
         opts = {'ipadx': 20, 'ipady': 20, 'sticky': 'nswe', "padx": 5, "pady": 5}
-        self.display = tk.Label(self, textvariable = self.current_number)
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+        self.display = tk.Label(self, textvariable = self.current_number, font = fontStyle , anchor="e")
         self.display.grid(row=0, column=0, columnspan=4, rowspan=2, **opts)
 
         # Buttons
@@ -63,6 +66,16 @@ class Calculator(tk.Frame):
         button.config(cursor="hand2", background="blue", foreground="white", bd=0)
         if content in "00123456789":
             button.config(command=partial(self.btn_number, content))
+        elif content == ",":
+            button.config(command=self.btn_comma_action)
+        elif content == "C":
+            button.config(command=self.btn_reset)
+        elif content == "R":
+            button.config(command=self.btn_remove_one)
+        elif content in "+-X/":
+            button.config(command=partial(self.btn_action, content))
+        elif content == "=":
+            button.config(command=self.btn_equal_action)
         button.grid(row=row, column=column, **opts)
         return button
 
@@ -70,9 +83,50 @@ class Calculator(tk.Frame):
     def btn_number(self, number):
         self.current_number.set(self.current_number.get() + number)
     
+    def btn_comma_action(self):
+        if "." not in self.current_number.get():
+            self.current_number.set(self.current_number.get() + ".")
 
     def btn_reset(self):
-        pass
+        self.current_number.set("")
+        self.aux_number = ""
+    
+    def btn_remove_one(self):
+        if self.current_number != "":
+            self.current_number.set(self.current_number.get()[:-1])
+
+    def btn_plus_action(self):
+        self.aux_number = self.current_number.get()
+        self.current_number.set("")
+        self.op = "+"
+    
+    def btn_action(self, option):
+        if option == "+":
+            self.aux_number = self.current_number.get()
+            self.current_number.set("")
+            self.op = "+"
+        elif option == "-":
+            self.aux_number = self.current_number.get()
+            self.current_number.set("")
+            self.op = "-"
+        elif option == "X":
+            self.aux_number = self.current_number.get()
+            self.current_number.set("")
+            self.op = "X"
+        elif option == "/":
+            self.aux_number = self.current_number.get()
+            self.current_number.set("")
+            self.op = "/"
+
+    def btn_equal_action(self):
+        if self.op == "+":
+            self.current_number.set(float(self.current_number.get()) + float(self.aux_number))
+        elif self.op == "-":
+            self.current_number.set(float(self.aux_number) - float(self.current_number.get()))
+        elif self.op == "X":
+            self.current_number.set(float(self.current_number.get()) * float(self.aux_number))
+        elif self.op == "/":
+            self.current_number.set(float(self.aux_number) / float(self.current_number.get()))
 
 if __name__ == "__main__":
     app = App()
